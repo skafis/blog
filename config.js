9c5a78d99eb371d19d4d430e004dae8ed9838c99
skafis <franciskipchumba5@gmail.com>
2016-01-23 18:23:36 +0300

    unsure files

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'https://dry-dawn-33055.herokuapp.com/',
+        url: 'https://francks-blog.herokuapp.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'https://dry-dawn-33055.herokuapp.com/',
+        url: 'https://francks-blog.herokuapp.com/',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
